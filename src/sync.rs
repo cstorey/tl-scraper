7fd13791d79b55f24bde7dd573efbcf3be3f8d9b
Ceri Storey <cez@necrofish.org.uk>
2021-10-06 13:44:04 +0100

    Store accounts by account number.

--- src/sync.rs
+++ src/sync.rs
@@ -5,7 +5,7 @@ use chrono::{Datelike, NaiveDate};
 use serde::Serialize;
 use tempfile::NamedTempFile;
 use tokio::task::block_in_place;
-use tracing::{debug, info, instrument, warn};
+use tracing::{debug, info, instrument};
 
 use crate::{
     client::{AccountsResult, CardsResult},
@@ -23,8 +23,8 @@ pub async fn run_sync(
     let accounts = scrape_accounts(&tl, target_dir).await?;
 
     for account in accounts {
-        scrape_account_balance(&tl, target_dir, &account.account_id).await?;
-        scrape_account_tx(&tl, target_dir, &account.account_id, from_date, to_date).await?;
+        scrape_account_balance(&tl, target_dir, &account).await?;
+        scrape_account_tx(&tl, target_dir, &account, from_date, to_date).await?;
     }
 
     let cards = scrape_cards(&tl, target_dir).await?;
@@ -51,38 +51,42 @@ async fn scrape_accounts(tl: &TlClient, target_dir: &Path) -> Result<Vec<Account
     Ok(accounts.results)
 }
 
-#[instrument(skip(tl, target_dir))]
-async fn scrape_account_balance(tl: &TlClient, target_dir: &Path, account_id: &str) -> Result<()> {
-    info!(%account_id, "Fetch balance");
-    let bal = tl.account_balance(account_id).await?;
-    write_atomically(
-        &target_dir
-            .join("accounts")
-            .join(&account_id)
-            .join("balance.json"),
-        &bal,
-    )
-    .await?;
+#[instrument(skip(tl, target_dir, account), fields(account_id=%account.account_id))]
+async fn scrape_account_balance(
+    tl: &TlClient,
+    target_dir: &Path,
+    account: &AccountsResult,
+) -> Result<()> {
+    info!("Fetch balance");
+    let bal = tl.account_balance(&account.account_id).await?;
+    let path = &target_dir
+        .join("accounts")
+        .join(&format!(
+            "{} {}",
+            account.account_number.sort_code, account.account_number.number
+        ))
+        .join("balance.json");
+    write_atomically(path, &bal).await?;
     Ok(())
 }
 
-#[instrument(skip(tl, target_dir))]
+#[instrument(skip(tl, target_dir, account, from_date, to_date), fields(account_id=%account.account_id))]
 async fn scrape_account_tx(
     tl: &TlClient,
     target_dir: &Path,
-    account_id: &str,
+    account: &AccountsResult,
     from_date: NaiveDate,
     to_date: NaiveDate,
 ) -> Result<()> {
-    info!(%account_id, ?from_date, ?to_date, "Fetch transactions");
+    info!(?from_date, ?to_date, "Fetch transactions");
     for (start_of_month, end_of_month) in months(from_date, to_date) {
-        debug!(%account_id, ?start_of_month, ?end_of_month, "Scrape month");
+        debug!(?start_of_month, ?end_of_month, "Scrape month");
         let mut txes = tl
-            .account_transactions(account_id, start_of_month, end_of_month)
+            .account_transactions(&account.account_id, start_of_month, end_of_month)
             .await?;
 
         if txes.results.is_empty() {
-            warn!("No results for month found");
+            info!(?start_of_month, "No results for month found");
             continue;
         }
 
@@ -91,7 +95,10 @@ async fn scrape_account_tx(
         write_atomically(
             &target_dir
                 .join("accounts")
-                .join(account_id)
+                .join(&format!(
+                    "{} {}",
+                    account.account_number.sort_code, account.account_number.number
+                ))
                 .join(start_of_month.format("%Y-%m.json").to_string()),
             &txes,
         )
@@ -138,7 +145,7 @@ async fn scrape_card_tx(
             .await?;
 
         if txes.results.is_empty() {
-            warn!("No results for month found");
+            info!(?start_of_month, "No results for month found");
             continue;
         }
 
