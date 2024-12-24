aff4c69c9325bbe9767f8af39cae9e2da293295e
Ceri Storey <git@noncrab.net>
2024-12-24 12:55:22 +0000

    Auto refresh access token when needed.

--- gocardless/src/auth.rs
+++ gocardless/src/auth.rs
@@ -28,6 +28,17 @@ pub(crate) struct Secrets {
     secret_key: String,
 }
 
+#[derive(Debug, Clone, Serialize)]
+pub(crate) struct TokenRefreshReq {
+    refresh: String,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub(crate) struct TokenRefreshResp {
+    access: String,
+    access_expires: i64,
+}
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub(crate) struct GCToken {
     access: String,
@@ -76,6 +87,15 @@ impl Token {
             refresh_expires: authed_at + Duration::seconds(gctoken.refresh_expires),
         }
     }
+
+    fn refreshed(&self, authed_at: DateTime<Utc>, refresh: &TokenRefreshResp) -> Token {
+        Token {
+            access: refresh.access.clone(),
+            access_expires: authed_at + Duration::seconds(refresh.access_expires),
+            refresh: self.refresh.clone(),
+            refresh_expires: self.refresh_expires,
+        }
+    }
 }
 
 #[instrument(skip_all, fields(?path))]
@@ -88,12 +108,41 @@ async fn store_token(path: &Path, tok: &Token) -> Result<()> {
 
 #[instrument(skip_all, fields(?path))]
 async fn load_token(path: &Path) -> Result<Token> {
-    let buf = tokio::fs::read(&path).await?;
-    let secrets = serde_json::from_slice(&buf)?;
+    let buf = tokio::fs::read(path).await?;
+    let mut token = serde_json::from_slice::<Token>(&buf)?;
+
+    let now = Utc::now();
+
+    if token.access_expires <= now {
+        debug!(expired_at=?token.access_expires, "Access token expired, refreshing");
+        token = refresh_token(&token).await?;
+
+        store_token(path, &token).await?;
+    }
 
     debug!(?path, "Loaded token");
 
-    Ok(secrets)
+    Ok(token)
+}
+
+#[instrument(skip_all)]
+async fn refresh_token(token: &Token) -> Result<Token> {
+    let client = BankDataClient::unauthenticated();
+
+    let authed_at = Utc::now();
+
+    let refresh = client
+        .post::<TokenRefreshResp>(
+            "/api/v2/token/refresh/",
+            &TokenRefreshReq {
+                refresh: token.refresh.clone(),
+            },
+        )
+        .await?;
+
+    let tok = token.refreshed(authed_at, &refresh);
+
+    Ok(tok)
 }
 
 #[instrument(skip_all, fields(?path))]
